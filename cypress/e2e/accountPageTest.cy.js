68566661b138d69f2f81dab68ab7cdf90ecd12af
Radosław K <radek.kilijanek@gmail.com>
2023-05-05 12:06:14 +0200

    add page object folder and new page in this folder, add new test accountPageTest

---
+++ cypress/e2e/accountPageTest.cy.js
@@ -0,0 +1,19 @@
+import HomePage from "../page object/homePage";
+import AccountPage from "../page object/myAccountPage";
+
+describe('Test Account page', () => {
+    const homePage = new HomePage();
+    const accountPage = new AccountPage();
+
+    before(function() {
+        cy.fixture('user').as('UserData')
+    })
+
+    it('should login to the application', function() {
+        homePage.visitPage()
+        homePage.clickMyAccountHeaderButton()
+        accountPage.fillUsernameFieldWithEmail(this.UserData.email)
+        accountPage.fillPasswordField(this.UserData.password)
+        accountPage.clickLoginButton()
+    })
+})
